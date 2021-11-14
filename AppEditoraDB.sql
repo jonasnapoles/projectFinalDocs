6c8586a8ec7fb2dc053219e44078174c39489d96
Jonas Oliveira <81438449+jonasnapoles@users.noreply.github.com>
2021-11-14 03:32:13 +0000

    Database
    Update database final project

---
+++ AppEditoraDB.sql
@@ -0,0 +1,46 @@
+-- APP EDITORA ---
+-- Final Project --
+
+CREATE DATABASE appEditora;
+
+USE appEditora;
+
+
+-- Criar primeiro as tabelas que não há FK.
+
+CREATE TABLE Writer(ID INT NOT NULL AUTO_INCREMENT,
+ Name VARCHAR(50) NOT NULL,
+ Surname VARCHAR(70) NOT NULL,
+ NIF VARCHAR(11) NOT NULL,
+ PRIMARY KEY(ID));
+
+
+------------
+
+CREATE TABLE Bookbinding(ID INT NOT NULL AUTO_INCREMENT,
+Name VARCHAR(50),
+PRIMARY KEY(ID));
+
+
+-------------
+
+CREATE TABLE Categories (ID INT NOT NULL AUTO_INCREMENT,
+Name VARCHAR(50),
+PRIMARY KEY(ID));
+
+----------------------------------------------------------------------------
+
+CREATE TABLE Book(ID INT NOT NULL AUTO_INCREMENT, Title VARCHAR (150) NOT NULL,
+ bookRelease DATE NOT NULL, 
+ ISBN VARCHAR(13) NOT NULL UNIQUE,
+  EAN VARCHAR(14) NOT NULL UNIQUE, 
+  Category_id INT, Bookbinding_id INT,
+  PRIMARY KEY (ID), FOREIGN KEY(Category_id) REFERENCES Categories(ID),
+  FOREIGN KEY(Bookbinding_id) REFERENCES Bookbinding(ID));
+  
+-- Media table
+CREATE TABLE bookWriter(Writer_id INT, Book_id INT, 
+FOREIGN KEY(Writer_id) REFERENCES Writer(ID),
+FOREIGN KEY(Book_id) REFERENCES Book(ID));
+  
+  
